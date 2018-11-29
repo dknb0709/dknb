d79542101c3d5b80fa1d2c6913085ac6530b15ba
dknb <ibot07092@google.com>
2018-11-29 23:34:50 +0900

    added test

--- parallel/thread/test.cpp
+++ parallel/thread/test.cpp
@@ -5,8 +5,7 @@
 
 int square(int n) { return n * n; }
 
-int main () {
-
+void test_use_directly() {
   std::vector<std::future<int>> results;
   {
     dknb::parallel::thread_pool pool(5);
@@ -18,7 +17,49 @@ int main () {
       result.wait();
       std::cout << result.get() << ' ';
     }
+    std::cout << std::endl;
   }
+}
+
+class test_composition {
+  std::vector<std::future<int>> results_;
+  std::shared_ptr<dknb::parallel::thread_pool> pool_;
+  struct worker_process {
+    int operator()(int n) {
+      return n * n;
+    }
+  };
+public:
+  test_composition(int threads)
+  : pool_(new dknb::parallel::thread_pool(threads))
+  { }
+  virtual ~test_composition()
+  { }
+
+  void request(int n) {
+    std::function<int()> f = std::bind(test_composition::worker_process(), n);
+    results_.push_back(pool_->enqueue(f));
+  }
+
+  void get_results() {
+    for (auto& result : results_) {
+      result.wait();
+      std::cout << result.get() << ' ';
+    }
+    std::cout << std::endl;
+  }
+};
+
+int main () {
+  std::cout << "test_use_directly" << std::endl;
+  test_use_directly();
+
+  std::cout << "test_composition" << std::endl;
+  test_composition com(3);
+  for (int i = 0; i < 8; ++i) {
+    com.request(i);
+  }
+  com.get_results();
 
   std::cin.get();
 }
